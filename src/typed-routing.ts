e60bcd9035091c3b575ff9df3df7b612c5a89a7b
Yunus Öztürk <yunusozturk@outlook.de>
2023-06-21 11:01:29 +0200

    Formatting

--- src/typed-routing.ts
+++ src/typed-routing.ts
@@ -67,20 +67,26 @@ const routesX = {
 
 type RouteDefinition = typeof routesX;
 
-
 type getPath<T extends RouteDef, Parent extends string = ''> =
-    T extends PageWithChildren ? getPath<T['children'], `${Parent}`> :
-    T extends Page ? `${Parent}` :
-    T extends { [key: string]: RouteDef } ? {
-        [P in keyof T]: getPath<T[P], `${Parent}/${P}`>
-    }[keyof T] : never;
+    T extends PageWithChildren 
+        ? getPath<T['children'], `${Parent}`> 
+        : T extends Page 
+            ? `${Parent}` 
+            : T extends { [key: string]: RouteDef } 
+                ? {
+                    [P in keyof T]: 
+                        P extends string ? 
+                            getPath<T[P], `${Parent}/${P}`> 
+                            : never
+                  }[keyof T] 
+                : never;
 
 type Route = getPath<RouteDefinition>;
 
 type Segments<T extends Route> = Split<T, '/'>;
 
 type RouteParams<T extends Route> = {
-    [P in Segments<T>[number]as P extends `:${infer PN}` ? (PN extends '' ? never : PN) : never]: string;
+    [P in Segments<T>[number] as P extends `:${infer PN}` ? (PN extends '' ? never : PN) : never]: string;
 };
 
 const navigate = <T extends Route>(route: T, params: RouteParams<T>) => {
@@ -90,4 +96,4 @@ const navigate = <T extends Route>(route: T, params: RouteParams<T>) => {
 navigate('/bestellen/:leadId', { leadId: '123414241' });
 navigate('/vergleich/ohne-hohe-ablehnungsquote/:hardwareIdentifier/acd', { hardwareIdentifier: 'iphone-14' });
 
-navigate('/vergleich/rufnummernmitnahme/:hardwareIdentifier', { hardwareIdentifier: 'iphone-14' })
+navigate('/vergleich/rufnummernmitnahme/:hardwareIdentifier', { hardwareIdentifier: 'iphone-14' });
