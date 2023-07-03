d65b369a669d6d17a90593dc2e7f5499c65601eb
Yunus Öztürk <yunusozturk@outlook.de>
2023-07-03 15:57:58 +0200

    Simplify the problem

--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -1,8 +1,3 @@
-type GetBoardValues<T extends string> = 
-    T extends `${infer Head}${infer Tail}` ? 
-        (Head extends BoardValue ? [Head, ...GetBoardValues<Tail>] : GetBoardValues<Tail>) :
-        [];
-
 type List<T = any> = ReadonlyArray<T>;
 
 type Length<List extends ReadonlyArray<any>> = List['length'];
@@ -50,57 +45,32 @@ type WinningCombinationIndices = [
     [2, 4, 6]
 ];
 
+type GetBoardValues<$Board extends string> = 
+    $Board extends `${infer FirstChar}${infer TailString}` 
+        ? FirstChar extends BoardValue 
+            ? [FirstChar, ...GetBoardValues<TailString>] 
+            : GetBoardValues<TailString>
+        : [];
+
 type BoardValues = GetBoardValues<Board>;
 
-type Index = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-type _GetBoardValueIndices<
+type GetBoardValuesForCombination<$Combination extends Combination> = 
+    BoardValues[$Combination[0]] |
+    BoardValues[$Combination[1]] |
+    BoardValues[$Combination[2]];
+    
+type _IsWinning<
         $Player extends Player, 
-        $Values extends BoardValue[], 
-        $CurrentIndex extends List<number>
+        $Combinations extends List<Combination>
     > = 
-    Length<$Values> extends 0 
-        ? [] 
-        : [
-            Head<$Values> extends $Player ? Head<$CurrentIndex> : -1, 
-            ..._GetBoardValueIndices<$Player, Tail<$Values>, Tail<$CurrentIndex>>
-          ];
+        Length<$Combinations> extends 0
+            ? false
+            : GetBoardValuesForCombination<Head<$Combinations>> extends $Player
+                ? true
+                : _IsWinning<$Player, Tail<$Combinations>>;
 
-type GetBoardValueIndices<$Player extends Player> = 
-    _GetBoardValueIndices<$Player, BoardValues, Index>;
-
-type GetSingleCombination<
-        $BoardIndices extends List<number>, 
-        $Combination extends Combination
-    > = [
-    $BoardIndices[$Combination[0]],
-    $BoardIndices[$Combination[1]],
-    $BoardIndices[$Combination[2]]
-];
-
-type GetAllCombinations<$Player extends Player> = 
-    _GetAllCombinations<GetBoardValueIndices<$Player>, WinningCombinationIndices>;
-
-type _GetAllCombinations<
-    $BoardIndices extends List<number>, 
-    $Combinations extends List<Combination>
-    > = 
-    Length<$Combinations> extends 0 
-        ? [] 
-        : [
-            GetSingleCombination<$BoardIndices, Head<$Combinations>>, 
-            ..._GetAllCombinations<$BoardIndices, Tail<$Combinations>> 
-        ];
-
-type _IsWinning<$BoardCombinations extends List<Combination>> = 
-    Length<$BoardCombinations> extends 0 ? 
-        false :
-        -1 extends Head<$BoardCombinations>[number] ? 
-            _IsWinning<Tail<$BoardCombinations>> : 
-            true;
-
-type IsWinning<$Player extends Player> = 
-    _IsWinning<GetAllCombinations<$Player>>;
+type IsWinning<$Player extends Player> =
+    _IsWinning<$Player, WinningCombinationIndices>;
 
 type _Winner = 
     (IsWinning<X> extends true 
